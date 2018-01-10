6e2ee933b5a5931346b7fd8b4b36bc6866b03a76
Auston Stewart <auston@shedbuilt.net>
2018-01-10 01:44:59 -0500

    Updating to 4.2.0

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/gawk-4.1.4
-cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${SHED_FAKEROOT}/usr/share/doc/gawk-4.1.4
+sed -i 's/extras//' Makefile.in
+./configure --prefix=/usr || exit 1
+make -j $SHED_NUMJOBS || exit 1
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
+mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
+cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
