6bdb24a570c08a5947f65a4e5f4637f9107b8a1d
Auston Stewart <auston@shedbuilt.net>
2018-01-12 01:38:49 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-sed -i 's/extras//' Makefile.in
-./configure --prefix=/usr || exit 1
-make -j $SHED_NUMJOBS || exit 1
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
-cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools || return 1
+        ;;
+    *)
+        sed -i 's/extras//' Makefile.in
+        ./configure --prefix=/usr || return 1
+        ;;
+esac
+
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
+
+if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
+    mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
+    cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
+fi
