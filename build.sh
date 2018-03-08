c6e5afa1e3a65794968003dd8f482afcf84da08f
Auston Stewart <auston@shedbuilt.net>
2018-03-08 08:22:55 -1000

    Updating to 4.2.1

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 case "$SHED_BUILDMODE" in
     toolchain)
-        ./configure --prefix=/tools || return 1
+        ./configure --prefix=/tools || exit 1
         ;;
     *)
         sed -i 's/extras//' Makefile.in
-        ./configure --prefix=/usr || return 1
+        ./configure --prefix=/usr || exit 1
         ;;
 esac
-
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
-
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
 if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
-    mkdir -pv ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
-    cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.0
+    mkdir -pv "${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.1"
+    cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} "${SHED_FAKEROOT}/usr/share/doc/gawk-4.2.1"
 fi
