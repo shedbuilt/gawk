b78b5341be20b13d80e968737a0d3c31c83686f1
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:02:01 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        ./configure --prefix=/tools || exit 1
-        ;;
-    *)
-        sed -i 's/extras//' Makefile.in
-        ./configure --prefix=/usr || exit 1
-        ;;
-esac
-make -j $SHED_NUM_JOBS && \
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_PREFIX='/usr'
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+fi
+SHED_PKG_LOCAL_DOCDIR=${SHED_PKG_LOCAL_PREFIX}/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}
+sed -i 's/extras//' Makefile.in &&
+./configure --prefix=${SHED_PKG_LOCAL_PREFIX} &&
+
+# Build and Install
+make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
-    mkdir -pv "${SHED_FAKE_ROOT}/usr/share/doc/gawk-4.2.1"
-    cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} "${SHED_FAKE_ROOT}/usr/share/doc/gawk-4.2.1"
+
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    mkdir -pv "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" &&
+    cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
 fi
